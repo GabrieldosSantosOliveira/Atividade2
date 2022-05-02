ff27ea60005892ca5073bf3dd5450e88290dc258
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-05-02 11:47:48 -0400

    Feito o roteamento e uma estilização basica

--- components/Logo.js
+++ components/Logo.js
@@ -1,9 +1,12 @@
 import styles from "/components/logo.module.css"
-export default function Logo(){
+import Link from 'next/link'
+export default function Logo() {
   return (
     <>
       <div className={styles.container}>
-        ANDRE VIOLIN
+        <Link href="/">
+          <a> ANDRE VIOLIN</a>
+        </Link>
       </div>
     </>
   )
