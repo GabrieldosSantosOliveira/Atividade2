f7648b8dbf4c9c26befc4b5b15444b59f45abc1f
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-13 23:19:20 -0400

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        app: 'url(/background.png)',
+      },
+      colors: {
+        background: '#000',
+        'text-color': '#FFF',
+        button: '#8257E5',
+        border: '#29292E',
+        'menu-mobile': '#A8A8B3',
+        'color-menu-mobile': '#E1E1E6',
+      },
+      fontFamily: {
+        manrope: "'Manrope', sans-serif;",
+      },
+    },
   },
   plugins: [],
 };
