60d59f561d03d4a693850275390bd91d08d7caf9
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-13 23:20:52 -0400

    add page home with style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,37 @@
+import { Header } from '@/components/Header';
+import { Heading } from '@/components/Heading';
 import Head from 'next/head';
+import Image from 'next/image';
 export default function Home() {
   return (
     <>
       <Head>
         <title>RocketCoffee</title>
       </Head>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="overflow-x-hidden max-w-full py-10 flex-col flex min-h-screen  bg-app bg-cover bg-no-repeat">
+        <Header />
+        <main className="pt-8 flex flex-col justify-center items-center text-5xl">
+          <Heading
+            fontWeight={700}
+            className="text-white lg:text-8xl text-center text-5xl"
+          >
+            Great Coffee
+          </Heading>
+          <Heading
+            fontWeight={700}
+            className="lg:text-8xl text-center text-shadow-heading"
+          >
+            &lt; Great Code / &gt;
+          </Heading>
+          <Image
+            src="/Coffee.png"
+            width={800}
+            height={500}
+            alt="Dois copos de café com alguns grãos de café ao redor "
+            className="object-contain w-full h-full max-w-[50rem] max-h-[31.25rem]"
+          />
+        </main>
+      </div>
     </>
   );
 }
